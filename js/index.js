e5ec0f5b8ec9e0583936d45be92f7ce29a68f2d5
Morshaedul <morshaedulanirbanit@gmail.com>
2022-07-21 00:34:07 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Create a Map
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+  
+  document.getElementById("demo").innerHTML = fruits.get("apples");
